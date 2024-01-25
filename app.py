e6b03d56a3127d926d3f9acc825d1a84e590edd3
Hari Meyyappan <hari.m.subbiah@gmail.com>
2024-01-25 10:23:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,5 @@ app = Flask(__name__)
 
 @app.get('/')
 def hello_world():
-    program = OpenAIPydanticProgram.from_defaults(
-    llm=OpenAI(temperature=0, model="gpt-4-0613", api_key="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"),
-    prompt_template_str=(
-        "Please extract the following spanish resume text into a structured data:"
-        " {input_str}. The column names are the following: ['Name', 'Email',"
-        " 'Skills', 'Experience']. Do not specify additional parameters that"
-        " are not in the function schema. "
-    ),
-    verbose=True,
-)
+    console.log("hello world"
     return "hello world"
